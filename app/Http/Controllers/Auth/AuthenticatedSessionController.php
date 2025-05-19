cb81bc25507d8873cf642d0bf89cc7a5478917d3
Silna Anil <silnaanil.nrs@gmail.com>
2025-05-19 11:53:43 +0530

    logout functionality add

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -39,12 +39,9 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request): JsonResponse
     {
-        Auth::guard('web')->logout();
+        $request->user()->currentAccessToken()->delete();
 
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
-        return response()->noContent();
+        return response()->json(['message' => 'Logged out']);
     }
+
 }
